e6cf0a20dd7b1557eb26a1a595e0f32a68e978cd
bellingson <ben@nofluffjuststuff.com>
2015-07-07 14:55:11 -0400

    adding size class example app

---
+++ SizeClassTest/SizeClassTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassTest
+//
+//  Created by Ben Ellingson on 7/7/15.
+//  Copyright (c) 2015 NNM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
